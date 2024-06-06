9b935f76e9e2a42623cdf63f6c7c38c90e5a06e6
Nectolyt <gabriel.boucneau@gmail.com>
2024-06-06 12:03:00 +0200

    page table

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,45 +1,55 @@
 'use strict';
 
+const tableau = document.getElementById('table_plonge');
 let select = document.getElementById('profondeur');
 const form = document.getElementById('form_p');
-const tableau = document.getElementById('table_plonge');
 const value = document.getElementById('value');
-ajaxRequest('GET','php/request.php/getProf/',remplireForm);
+
+let profondeur = 0;
+
+ajaxRequest('GET','php/request.php/getPlongees/',remplireForm);
 
 function remplireForm(donnee) {
     let size = Object.keys(donnee).length;
-    console.log(size);
-    for (let i = 0; i < size-1; i++) {
-        select.options[select.options.length] = new Option(donnee[i]["prof"], donnee[i]["prof"]);
+    for (let i = 0; i < size; i++) {
+        select.options[select.options.length] = new Option(donnee[i]["profondeur"], donnee[i]["profondeur"]);
     }
 }
 
 form.addEventListener('change', function handleChange(event) {
-    ajaxRequest('GET','php/request.php/dbGetTable/'+event.target.value,remplireTableau);
+    profondeur = event.target.value;
+    ajaxRequest('GET','php/request.php/getPlongee/'+event.target.value,remplireTableau);
+    
 });
 
+
 function remplireTableau(donnee) {
+    document.getElementById('infos').innerHTML = '';
     clearTableau();
-    let size = Object.keys(donnee).length;
-    let data = "";
-    for (let i = 0; i < size; i++) {
-        data += '<tr>'
-        data += '<td>' + donnee[i]["prof"] + '</td>';
-        data += '<td>' + donnee[i]["t"] + '</td>';
-        data += '<td>' + decideNull(donnee[i]["m15"]) + '</td>';
-        data += '<td>' + decideNull(donnee[i]["m12"]) + '</td>';
-        data += '<td>' + decideNull(donnee[i]["m9"]) + '</td>';
-        data += '<td>' + decideNull(donnee[i]["m6"]) + '</td>';
-        data += '<td>' + decideNull(donnee[i]["m3"]) + '</td>';
-        data += '<td>' + donnee[i]["dtr"] + '</td>';
-        data += '<td>' + donnee[i]["gps"] + '</td>';
-        data += '</tr>';
+    if (profondeur != 'x') {
+        let size = Object.keys(donnee).length;
+        let data = "";
+        for (let i = 0; i < size; i++) {
+            data += '<tr>'
+            data += '<td>' + decideNull(donnee[i]["prenom_user"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["nom_user"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["profondeur"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["duree"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["bar_initial"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["volume_initial"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["note"]) + '</td>';
+            data += '<td>' + decideNull(donnee[i]["description"]) + '</td>';
+            data += '</tr>';
+        }
+        tableau.innerHTML += data;
     }
-    tableau.innerHTML += data;
+    else {
+        document.getElementById('infos').innerHTML = 'Veuillez entrer une profondeur valide.'
+    }
 }
 
 function decideNull(donnee) {if (donnee) {return donnee;}return '-';}
 
 function clearTableau() {
-    tableau.innerHTML = '<tr><th>Profondeur</th><th>Durée</th><th>Paliers 15m</th><th>Paliers 12m</th><th>Paliers 9m</th><th>Paliers 6m</th><th>Paliers 3m</th><th>DTR</th><th>GPS</th></tr>'
+    tableau.innerHTML = '<tr><th>Prénom</th><th>Nom</th><th>Profondeur</th><th>Durée</th><th>Pression initiale</th><th>Volume initiale</th><th>Note</th><th>Description</th></tr>'
 }
