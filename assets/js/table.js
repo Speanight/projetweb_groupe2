906987c8255bb771d66d3a5e09c22d2916d31c9a
Nectolyt <gabriel.boucneau@gmail.com>
2024-06-07 09:17:00 +0200

    update de la page table

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const tableau = document.getElementById('table_plonge');
 let select = document.getElementById('profondeur');
 const form = document.getElementById('form_p');
+const tableau = document.getElementById('table_plonge');
 const value = document.getElementById('value');
 
 let profondeur = 0;
 
-ajaxRequest('GET','php/request.php/getPlongees/',remplireForm);
+ajaxRequest('GET','php/request.php/getProf/',remplireForm);
 
 /**
  * Fonction permettant de remplir le formulaire de select avec la profondeur de toutes les plongées qui ont été effectué.
@@ -15,20 +15,22 @@ ajaxRequest('GET','php/request.php/getPlongees/',remplireForm);
  */
 function remplireForm(donnee) {
     let size = Object.keys(donnee).length;
-    for (let i = 0; i < size; i++) {
-        select.options[select.options.length] = new Option(donnee[i]["profondeur"], donnee[i]["profondeur"]);
+    console.log(size);
+    for (let i = 0; i < size-1; i++) {
+        select.options[select.options.length] = new Option(donnee[i]["prof"] + 'm', donnee[i]["prof"]);
     }
 }
 
 form.addEventListener('change', function handleChange(event) {
     profondeur = event.target.value;
-    ajaxRequest('GET','php/request.php/getPlongee/'+event.target.value,remplireTableau);
+    console.log(profondeur);
+    ajaxRequest('GET','php/request.php/dbGetTable/'+event.target.value,remplireTableau);
     
 });
 
 /**
  * Fonction permettant de remplir le tableau des valeurs que la requète ajax renverra.
- * @param {Array} values Tableau de dictionnaire
+ * @param {Array} donnee Tableau de dictionnaire
  */
 function remplireTableau(donnee) {
     document.getElementById('infos').innerHTML = '';
@@ -37,15 +39,16 @@ function remplireTableau(donnee) {
         let size = Object.keys(donnee).length;
         let data = "";
         for (let i = 0; i < size; i++) {
-            data += '<tr>'
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["prenom_user"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["nom_user"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["profondeur"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["duree"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["bar_initial"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["volume_initial"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["note"]) + '</td>';
-            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["description"]) + '</td>';
+            data += '<tr class="col-xs-1 text-center">'
+            data += '<td class="col-xs-1 text-center">' + donnee[i]["prof"] + '</td>';
+            data += '<td class="col-xs-1 text-center">' + donnee[i]["t"] + '</td>';
+            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["m15"]) + '</td>';
+            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["m12"]) + '</td>';
+            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["m9"]) + '</td>';
+            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["m6"]) + '</td>';
+            data += '<td class="col-xs-1 text-center">' + decideNull(donnee[i]["m3"]) + '</td>';
+            data += '<td class="col-xs-1 text-center">' + donnee[i]["dtr"] + '</td>';
+            data += '<td class="col-xs-1 text-center">' + donnee[i]["gps"] + '</td>';
             data += '</tr>';
         }
         tableau.innerHTML += data;
@@ -55,13 +58,12 @@ function remplireTableau(donnee) {
     }
 }
 
-
 /**
  * Fonction permettant de remplacer une case vide de la table par un '-' pour améliorer la lisibilité du tableau pour le client
- * @param {Array} values Valeur
+ * @param values Valeur
  */
 function decideNull(donnee) {if (donnee) {return donnee;}return '-';}
 
 function clearTableau() {
-    tableau.innerHTML = '<tr><th class="col-xs-1 text-center">Prénom</th><th class="col-xs-1 text-center">Nom</th><th class="col-xs-1 text-center">Profondeur</th><th class="col-xs-1 text-center">Durée</th><th class="col-xs-1 text-center">Pression initiale</th><th class="col-xs-1 text-center">Volume initiale</th><th class="col-xs-1 text-center">Note</th><th class="col-xs-1 text-center">Description</th></tr>'
+    tableau.innerHTML = '<tr><th class="col-xs-1 text-center">Profondeur</th><th class="col-xs-1 text-center">Durée</th><th class="col-xs-1 text-center">Paliers 15m</th><th class="col-xs-1 text-center">Paliers 12m</th><th class="col-xs-1 text-center">Paliers 9m</th><th class="col-xs-1 text-center">Paliers 6m</th><th class="col-xs-1 text-center">Paliers 3m</th><th class="col-xs-1 text-center">DTR</th><th class="col-xs-1 text-center">GPS</th></tr>'
 }
